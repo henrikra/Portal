0e9399036dc9aac5008ef93654bb7bbdb234cc65
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-05-27 23:06:50 +0300

    Make orange portal work

--- Assets/BluePortal.cs
+++ Assets/BluePortal.cs
@@ -15,7 +15,7 @@ public class BluePortal : MonoBehaviour {
 	}
 
     private void OnCollisionEnter(Collision collision) {
-        var otherPortal = GameObject.Find("Orange Portal").transform;
+        var otherPortal = GameObject.Find(this.gameObject.name == "Blue Portal" ? "Orange Portal" : "Blue Portal").transform;
         collision.gameObject.transform.position = otherPortal.position + otherPortal.up * .4f;
         var newAngle = otherPortal.up * collision.relativeVelocity.magnitude;
         collision.rigidbody.AddForce(newAngle, ForceMode.Impulse);
