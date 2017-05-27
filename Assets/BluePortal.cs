503178a01de1037ebac76b734dbaa975fb8bac4c
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-05-27 22:53:15 +0300

    Add dynamic force according to collision velocity

--- Assets/BluePortal.cs
+++ Assets/BluePortal.cs
@@ -6,7 +6,7 @@ public class BluePortal : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+        GetComponent<Renderer>().material.color = this.gameObject.name == "Blue Portal" ? new Color(0, 0, 255) : new Color(255, 140, 0);
 	}
 	
 	// Update is called once per frame
@@ -15,10 +15,9 @@ public class BluePortal : MonoBehaviour {
 	}
 
     private void OnCollisionEnter(Collision collision) {
-        //collision.collider.
-        Debug.Log(collision.gameObject.name);
-        var collisionObjectPosition = collision.gameObject.transform.position;
-        collision.gameObject.transform.position = new Vector3(collisionObjectPosition.x, collisionObjectPosition.y, collisionObjectPosition.z + 2);
-        collision.rigidbody.AddForce(collision.impulse, ForceMode.Impulse);
+        var otherPortal = GameObject.Find("Orange Portal").transform;
+        collision.gameObject.transform.position = otherPortal.position + otherPortal.up * .4f;
+        var newAngle = otherPortal.up * collision.relativeVelocity.magnitude;
+        collision.rigidbody.AddForce(newAngle, ForceMode.Impulse);
     }
 }
