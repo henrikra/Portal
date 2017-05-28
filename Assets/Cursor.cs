6a00f98470bd6cf2471d08a93c753c7d3dd5687f
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-05-28 14:04:27 +0300

    Shoot portals

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -6,6 +6,7 @@ using UnityEngine.VR.WSA.Input;
 public class Cursor : MonoBehaviour {
     private GameObject lastHitGameObject;
     GestureRecognizer recognizer;
+    private bool shouldShootBluePortal = true;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +18,28 @@ public class Cursor : MonoBehaviour {
     private void ToggleCubeGrab(InteractionSourceKind source, int tapCount, Ray headRay) {
         if (lastHitGameObject) {
             lastHitGameObject.SendMessage("OnGrab");
+        }
+        else {
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+            RaycastHit hitInfo;
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo)) {
+                if (shouldShootBluePortal && GameObject.Find("Blue Portal")) {
+                    Destroy(GameObject.Find("Blue Portal"));
+                }
+                else if (!shouldShootBluePortal && GameObject.Find("Orange Portal")) {
+                    Destroy(GameObject.Find("Orange Portal"));
+                }
+                var newPortal = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                newPortal.transform.localScale = new Vector3(.6f, .02f, .9f);
+                newPortal.transform.position = hitInfo.point;
+                newPortal.GetComponent<Renderer>().material.color = shouldShootBluePortal ? new Color(0, 0, 255) : new Color(255, 140, 0);
+                newPortal.transform.rotation = hitInfo.transform.rotation;
+                newPortal.name = shouldShootBluePortal ? "Blue Portal" : "Orange Portal";
+                newPortal.AddComponent<BluePortal>();
+
+                shouldShootBluePortal = !shouldShootBluePortal;
+            }
         }
     }
 
